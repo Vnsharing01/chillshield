966c783d03d8591db0ad9289c56a525b98367388
thicmn <thichu08@gmail.com>
2025-08-11 23:09:47 +0700

    add plash & app icon; remove redundant files

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,9 @@ import 'package:chillshield/shared/constants/app_clolors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Future.delayed(const Duration(seconds: 3));
   runApp(const MyApp());
 }
 

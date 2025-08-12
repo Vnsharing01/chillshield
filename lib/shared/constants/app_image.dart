7b06ca71d83401dcc39c1d416f3943d32cfeb898
thicmn <thichu08@gmail.com>
2025-08-12 23:52:57 +0700

    update icon app

--- lib/shared/constants/app_image.dart
+++ lib/shared/constants/app_image.dart
@@ -1,7 +1,7 @@
 class AppImage {
   static const String logo = 'assets/images/demo_logo.png';
   static const String plash = 'assets/images/plash_logo.png';
-  static const String icon = 'assets/images/icon.png';
+  static const String icon = 'assets/images/logo_app.png';
   static const String placeholder = 'assets/images/placeholder.png';
   static const String errorImage = 'assets/images/error_image.png';
   static const String splashScreen = 'assets/images/splash_screen.png';

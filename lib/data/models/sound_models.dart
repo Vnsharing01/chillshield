717933249ea731c4a2ed03a715c02723393cb527
thicmn <thichu08@gmail.com>
2025-08-22 17:49:07 +0700

    add lib hive to model

--- lib/data/models/sound_models.dart
+++ lib/data/models/sound_models.dart
@@ -1,9 +1,23 @@
-class SoundModels {
-  final int ?soundId;       // id âm thanh
-  final String ?soundType;  // loại âm thanh (tiếng suối, tiếng mưa)
-  final String ?soundName;  // tên âm thanh
-  final String ?soundPath;  // đường dẫn âm thanh
-  final String ?frequency;  // tần số âm thanh
+import 'package:copy_with_extension/copy_with_extension.dart';
+import 'package:hive/hive.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'sound_models.g.dart';
+
+@HiveType(typeId: 0)
+@JsonSerializable()
+@CopyWith()
+class SoundModels extends HiveObject {
+  @HiveField(0)
+  final int? soundId; // id âm thanh
+  @HiveField(1)
+  final String? soundType; // loại âm thanh (tiếng suối, tiếng mưa)
+  @HiveField(2)
+  final String? soundName; // tên âm thanh
+  @HiveField(3)
+  final String? soundPath; // đường dẫn âm thanh
+  @HiveField(4)
+  final String? frequency; // tần số âm thanh
 
   SoundModels({
     this.soundId,
@@ -13,22 +27,8 @@ class SoundModels {
     this.frequency,
   });
 
-  factory SoundModels.fromJson(Map<String, dynamic> json) {
-    return SoundModels(
-      soundId: json['sound_id'] as int?,
-      soundType: json['sound_type'] as String?,
-      soundName: json['sound_name'] as String?,
-      soundPath: json['sound_path'] as String?,
-      frequency: json['frequency'] as String?,
-    );
-  }
-  Map<String, dynamic> toJson() {
-    return {
-      'sound_id': soundId,
-      'sound_type': soundType,
-      'sound_name': soundName,
-      'sound_path': soundPath,
-      'frequency': frequency,
-    };
-  }
+  factory SoundModels.fromJson(Map<String, dynamic> json) =>
+      _$SoundModelsFromJson(json);
+
+  Map<String, dynamic> toJson() => _$SoundModelsToJson(this);
 }
